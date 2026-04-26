b1687647b1d3806993a7421c649e1d421bf2325e
Drayush Yadav <drayushyadav@Drayushs-MacBook-Pro-2.local>
2026-04-26 08:50:22 +0000

    Apr 26, 2026, 4:50 AM

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import os
 import pandas as pd
-from data_manager import load_trades, save_trades, add_trade, delete_trade
+from data_manager import load_trades, save_trades, add_trade, delete_trade, edit_trade
 
 # This block helps me move my file without having to change names each time.
 APP_PATH = os.path.dirname(os.path.abspath(__file__))
