bcb5a2891f23bb8d83650f6f92ac4cf68d61a863
Drayush Yadav <drayushyadav@Drayushs-MacBook-Pro-2.local>
2026-04-26 01:32:23 -0400

    Created CSV file to store data

--- src/main.py
+++ src/main.py
@@ -5,13 +5,13 @@ import matplotlib.pyplot as plt
 import yfinance as yf
 import os
 
-
+# This block helps me move my file without having to change names each time.
 APP_PATH = os.path.dirname(os.path.abspath(__file__))
 
 def get_data_path(filename: str) -> str:
     '''Returns the path to an asset file, given its filename.'''
     return os.path.join(APP_PATH, "data", filename)
 
-
+TRADES_FILE = get_data_path("trades.csv")
 
 
