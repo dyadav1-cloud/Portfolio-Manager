a02a446bfc0471f610d58a236a4fabe1b200e06c
Drayush Yadav <drayushyadav@Drayushs-MacBook-Pro-2.local>
2026-05-06 05:27:27 +0000

    May 6, 2026, 1:27 AM

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import pandas as pd
 from data_manager import load_trades, save_trades, add_trade, delete_trade, edit_trade
 from market_data import get_prices_for_tickers
 from analytics import calculate_portfolio_summary, calculate_position_metrics
-from charts import plot_allocation_donut
+from charts import plot_allocation_donut, plot_profit_loss_bar
 
 # This block helps me move my file without having to change names each time.
 APP_PATH = os.path.dirname(os.path.abspath(__file__))
