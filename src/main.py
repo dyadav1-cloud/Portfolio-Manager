112ef216b575e55594efda3ef20a0dae961ee940
Drayush Yadav <drayushyadav@Drayushs-MacBook-Pro-2.local>
2026-05-09 05:48:22 +0000

    May 9, 2026, 1:48 AM

--- src/main.py
+++ src/main.py
@@ -26,7 +26,12 @@ def get_data_path(filename: str) -> str:
     '''Returns the path to an asset file, given its filename.'''
     return os.path.join(APP_PATH, "data", filename)
 
-st.set_page_config(page_title="Portfolio Manager", page_icon="📈", layout="wide")
+st.set_page_config(
+    page_title="Portfolio Manager",
+    page_icon="📈",
+    layout="wide",
+    initial_sidebar_state="collapsed"
+)
 
 TRADES_FILE = get_data_path("trades.csv")
 
